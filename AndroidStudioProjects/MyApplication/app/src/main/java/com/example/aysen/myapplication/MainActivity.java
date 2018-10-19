0e69fb03ac50fc0158de6728d523884ea650af40
Aysen1 <aysenacun@gmail.com>
2018-10-20 00:07:31 +0300

    mini arayüz

--- AndroidStudioProjects/MyApplication/app/src/main/java/com/example/aysen/myapplication/MainActivity.java
+++ AndroidStudioProjects/MyApplication/app/src/main/java/com/example/aysen/myapplication/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.aysen.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.cevap);
+        Button b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        tv.setText(R.string.bilmece_cevabi);
     }
 }
