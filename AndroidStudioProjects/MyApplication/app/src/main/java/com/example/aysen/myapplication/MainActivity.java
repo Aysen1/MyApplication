f2180ad77d37571813b73a2fcfc49822eafd612a
Aysen1 <aysenacun@gmail.com>
2018-10-20 10:59:16 +0300

    karaktere ozellik tanımlama işlemleri

--- AndroidStudioProjects/MyApplication/app/src/main/java/com/example/aysen/myapplication/MainActivity.java
+++ AndroidStudioProjects/MyApplication/app/src/main/java/com/example/aysen/myapplication/MainActivity.java
@@ -2,17 +2,24 @@ package com.example.aysen.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, EditText.OnEditorActionListener {
     TextView tv;
     TextView tvKarakter_Ozellikleri;
     Button bSaldiri;
     Button bYemek;
     Button bUyku;
     karakter k;
+    EditText isim;
+    LinearLayout butonlar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,8 +36,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         k.kilo = 10;
         k.saldiri_gucu = 50;
         k.hareket_Sayisi = 15;
+        //tvKarakter_Ozellikleri.setText(k.toString());
+        tv.setText("Oyuna Hoşgeldiniz! Lütfen Bir İsim Belirleyiniz!");
+        isim = (EditText) findViewById(R.id.editText);
+        isim.setOnEditorActionListener(this);
+        butonlar = (LinearLayout) findViewById(R.id.butonlar);
+    }
+    @Override
+    public boolean onEditorAction(TextView tv1, int i, KeyEvent ke){
+        tv.setText("Karakterin ismi '"+isim.getText()+"' olarak atandı.");
+        k.isim = isim.getText().toString();
+        isim.setVisibility(View.INVISIBLE);
         tvKarakter_Ozellikleri.setText(k.toString());
-        tv.setText("Oyuna Hoşgeldiniz! Lütfen Bir Eylem Belirleyiniz!");
+        butonlar.setVisibility(View.VISIBLE);
+        return  true;
     }
 
     @Override
