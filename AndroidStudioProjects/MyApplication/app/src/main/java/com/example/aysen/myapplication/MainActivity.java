7fe6fa919c491ca928e365a7c657a671d84423ae
Aysen1 <aysenacun@gmail.com>
2018-10-20 09:28:45 +0300

    oyuna geciş

--- AndroidStudioProjects/MyApplication/app/src/main/java/com/example/aysen/myapplication/MainActivity.java
+++ AndroidStudioProjects/MyApplication/app/src/main/java/com/example/aysen/myapplication/MainActivity.java
@@ -8,17 +8,39 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     TextView tv;
+    TextView tvKarakter_Ozellikleri;
+    Button bSaldiri;
+    Button bYemek;
+    Button bUyku;
+    karakter k;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv = (TextView) findViewById(R.id.cevap);
-        Button b = (Button) findViewById(R.id.button);
-        b.setOnClickListener(this);
+        bSaldiri = (Button) findViewById(R.id.saldir);
+        bSaldiri.setOnClickListener(this);
+        bYemek = (Button) findViewById(R.id.yemek);
+        bYemek.setOnClickListener(this);
+        bUyku = (Button) findViewById(R.id.uyu);
+        bUyku.setOnClickListener(this);
+        tvKarakter_Ozellikleri = (TextView) findViewById(R.id.karakterozelligi);
+        k = new karakter();
+        k.kilo = 10;
+        k.saldiri_gucu = 50;
+        k.hareket_Sayisi = 15;
+        tvKarakter_Ozellikleri.setText(k.toString());
+        tv.setText("Oyuna Hoşgeldiniz! Lütfen Bir Eylem Belirleyiniz!");
     }
 
     @Override
     public void onClick(View v) {
-        tv.setText(R.string.bilmece_cevabi);
+        if (v.getId() == bYemek.getId())
+            tv.setText(k.yemek());
+        else if (v.getId() == bUyku.getId())
+            tv.setText(k.uyumak());
+        else if (v.getId() == bSaldiri.getId())
+            tv.setText(k.saldiri());
+        tvKarakter_Ozellikleri.setText(k.toString());
     }
 }
